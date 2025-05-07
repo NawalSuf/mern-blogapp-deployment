1ecd9059f39dc84fbbec154198b49367909cf79a
SDA2039-Nawal <alsufyaninawal@gmail.com>
2025-05-07 12:34:48 +0300

    Removed MongoDB URI from .env

--- backend/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-const morgan = require('morgan');
-const fs = require('fs');
-
-const date = new Date().toISOString().split('T')[0]
-
-module.exports = morgan('combined', {
-    stream: fs.createWriteStream(`./logs/${date}.log`, {flags:'a+'}),
-})
